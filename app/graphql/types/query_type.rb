6b23dfdd5285acbf646fb2e275c4fc000704e9b7
Théo Liberman <liberman.theo@gmail.com>
2019-06-16 18:53:08 +0200

    Add GraphQL queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,12 +3,47 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-          description: "An example field added by the generator"
+    field :orders, [OrderType], null: false
 
-    def test_field
-      "Hello World!"
+    def orders
+      Order.all
     end
+
+    field :order, OrderType, null: false do
+      argument :order_id, Int, required: true
+    end
+
+    def order(order_id:)
+      Order.find_by_order_id(order_id)
+    end
+
+    field :clients, [ClientType], null: false
+
+    def clients
+      Client.all
+    end
+
+    field :client, ClientType, null: false do
+      argument :client_code, Int, required: true
+    end
+
+    def client(client_code:)
+      Client.find_by_client_code(client_code)
+    end
+
+    field :items, [ItemType], null: false
+
+    def items
+      Item.all
+    end
+
+    field :item, ItemType, null: false do
+      argument :item_code, Int, required: true
+    end
+
+    def item(item_code:)
+      Item.find_by_item_code(item_code)
+    end
+
   end
 end
